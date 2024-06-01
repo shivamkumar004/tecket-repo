86321fda9a947e863ef814de84d2fd667e038841
shivamkumar004 <shivamkumar91284@gmail.com>
2024-06-01 18:16:37 +0530

    half done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ /* let str="hello";
+  let str2="world";
+
+  if(str==="hello" &&str2==="hello"){ 
+    console.log("hello world");
+     }else{
+      console.log('hi');
+     }*/
+     const employ= { 
+     name:"shivam",
+     salary:10,
+     emid :123,
+     };
+     if(employ.emid === 123){
+      console.log(employ);
+     }else{
+      console.log("bye");
+     }
+     const employ2= {
+      name:"shivam",
+      salary:20,
+      emid :132
+     }
+     if(employ.name===employ2.name){
+      console.log("hellow")}
+      else{ 
+        console.log("bye")
+      }
+      const fun1 = () =>{
+        console.log("1");
+      }
+      if(employ.name===employ.name)
+        { 
+          fun1()
+        }
+         else{
+          console.log("hello")
+        }
+     
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +60,6 @@ function App() {
       </header>
     </div>
   );
-}
 
+}
 export default App;
