062645419acd1e03164c16bd7d0f1fcb8a3031eb
shivamkumar004 <shivamkumar91284@gmail.com>
2024-06-11 17:33:02 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-//const [count, setCount] = useState(0) 
- const [data,setdata]=useState<string>("4");  
+const [data, setData] = useState<number>(0) ;
+const[numberData,setNumberData] = useState<number>(0);
+
  useEffect(() => {
-   setdata("world");
- },[]);//pre define function are called hook
+  addData();
+ },[numberData]);
+ const addData = () =>{
+  const oldData = data;
+  const newData = oldData +1;
+  setData(newData);
+  console.log(newData);
+ };
+ const decrementData = () => {
+  setNumberData(numberData-1);
+ };
 
   return (
     <>
-      <div>[data]</div>
+    <button title="title"onClick={addData} className={`number${data}`}>
+      add Data
+    </button>
+    <div>Incement data: {data} </div>
+    <br/>
+
+     <button onClick={decrementData}>decrement</button>
+     <div>decrement data :{numberData}</div>
       
     </>
-  )
+  );
 }
 
 export default App
